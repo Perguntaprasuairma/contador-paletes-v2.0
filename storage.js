a18f40076eec1adc6cad773711fc92709cb72e47
Perguntaprasuairma <HyuukU@gmail.com>
2026-08-02 23:02:43 -0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,19 @@
 
+const STORAGE_KEY = "LHCONTROL_V4";
+
+function carregarDados(){
+
+    const dados = localStorage.getItem(STORAGE_KEY);
+
+    return dados ? JSON.parse(dados) : [];
+
+}
+
+function salvarDados(dados){
+
+    localStorage.setItem(
+        STORAGE_KEY,
+        JSON.stringify(dados)
+    );
+
+}
