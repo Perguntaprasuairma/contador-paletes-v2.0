d584e3e5138990bec383b64b29fdbf4fdfde8f1d
Perguntaprasuairma <HyuukU@gmail.com>
2026-08-02 23:03:10 -0300

    Update history.js

--- history.js
+++ history.js
@@ -1,1 +1,15 @@
+const HISTORY_KEY = "LH_HISTORY";
 
+function salvarHistorico(item){
+
+    let lista =
+    JSON.parse(localStorage.getItem(HISTORY_KEY)) || [];
+
+    lista.unshift(item);
+
+    localStorage.setItem(
+        HISTORY_KEY,
+        JSON.stringify(lista)
+    );
+
+}
