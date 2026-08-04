0e56651c422f85dfbc1db49cda3d22bd9b38b126
Perguntaprasuairma <HyuukU@gmail.com>
2026-08-04 01:14:39 -0300

    Update Stage.js

--- Stage.js
+++ Stage.js
@@ -1,1 +1,224 @@
+/* ==========================================
+   LH CONTROL V4.2
+   stage.js
+========================================== */
 
+
+let stage = [];
+
+
+
+function iniciarStage(){
+
+    stage = Storage.carregarStage();
+
+    renderizarStage();
+
+}
+
+
+
+
+
+function renderizarStage(){
+
+    const container = document.getElementById("regions");
+
+
+    if(!container) return;
+
+
+    container.innerHTML = "";
+
+
+
+    stage.forEach(regiao => {
+
+
+        const card = document.createElement("div");
+
+
+        card.className = "card";
+
+
+        card.style.borderLeftColor = regiao.cor;
+
+
+
+        if(regiao.prioridade && regiao.valor > 0){
+
+            card.classList.add("prioridade-alta");
+
+        }
+
+
+
+        card.innerHTML = `
+
+
+        <div class="topo">
+
+
+            <div class="nome">
+
+                ${regiao.nome}
+
+                <br>
+
+                <small>${regiao.codigo}</small>
+
+            </div>
+
+
+            <div class="valor">
+
+                ${regiao.valor}
+
+            </div>
+
+
+        </div>
+
+
+
+
+        <div class="botoes">
+
+
+            <button class="menos"
+            onclick="alterarStage(${regiao.id},-10)">
+            -10
+            </button>
+
+
+            <button class="menos"
+            onclick="alterarStage(${regiao.id},-5)">
+            -5
+            </button>
+
+
+            <button class="menos"
+            onclick="alterarStage(${regiao.id},-1)">
+            -1
+            </button>
+
+
+            <button class="mais"
+            onclick="alterarStage(${regiao.id},1)">
+            +1
+            </button>
+
+
+            <button class="mais"
+            onclick="alterarStage(${regiao.id},5)">
+            +5
+            </button>
+
+
+            <button class="mais"
+            onclick="alterarStage(${regiao.id},10)">
+            +10
+            </button>
+
+
+        </div>
+
+
+        `;
+
+
+        container.appendChild(card);
+
+
+    });
+
+
+
+    atualizarTotalStage();
+
+    atualizarPrioridades();
+
+
+}
+
+
+
+
+
+function alterarStage(id, quantidade){
+
+
+
+    const destino = stage.find(
+
+        r => r.id === id
+
+    );
+
+
+
+    if(!destino) return;
+
+
+
+    destino.valor += quantidade;
+
+
+
+    if(destino.valor < 0){
+
+        destino.valor = 0;
+
+    }
+
+
+
+    Storage.salvarStage(stage);
+
+
+
+    renderizarStage();
+
+
+    atualizarDashboard();
+
+
+}
+
+
+
+
+
+
+
+
+function atualizarTotalStage(){
+
+
+
+    const campo =
+    document.getElementById(
+        "totalPallets"
+    );
+
+
+
+    if(!campo) return;
+
+
+
+    const total = stage.reduce(
+
+        (soma,r)=> soma+r.valor,
+
+        0
+
+    );
+
+
+
+    campo.innerText = total;
+
+
+
+}
